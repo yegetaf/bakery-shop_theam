2f15a6266e16e39b8a613023ae8d54c16551fedb
yegetaf <37799220+yegetaf@users.noreply.github.com>
2021-05-26 01:10:02 +0300

    Add files via upload
    front page

--- bakery-shop_theam/front-page.php
+++ bakery-shop_theam/front-page.php
@@ -1,50 +1,153 @@
 <?php
-/**
- * The front page template file
- *
- * If the user has selected a static page for their homepage, this is what will
- * appear.
- * Learn more: https://codex.wordpress.org/Template_Hierarchy
- *
- * @package Bakery_Shop
- * @since 1.0.1
- * @version 1.0.3
- */
-
-$slider_enable       = get_theme_mod( 'bakery_shop_ed_slider','1' );
-$featured_enable     = get_theme_mod( 'bakery_shop_ed_featured_section', '1' );
-$welcome_enable      = get_theme_mod( 'bakery_shop_ed_welcome_section','1' );
-$blog_enable         = get_theme_mod( 'bakery_shop_ed_blog_section','1' );
-$products_enable     = get_theme_mod( 'bakery_shop_ed_product_section', '1' );
-$cta_enable          = get_theme_mod( 'bakery_shop_ed_cta_section', '1' );
-$teams_enable        = get_theme_mod( 'bakery_shop_ed_teams_section', '1' );
-$testimonials_enable = get_theme_mod( 'bakery_shop_ed_testimonials_section', '1' );
-
-get_header(); 
-           
-    if ( 'posts' == get_option( 'show_on_front' ) ) {
-        include( get_home_template() );
-    }elseif( $slider_enable || $featured_enable || $welcome_enable || $products_enable || $blog_enable || $cta_enable || $teams_enable || $testimonials_enable ){ ?>
-        
-        <?php
-        /**
-         * Home Page Contents
-         * 
-         * @hooked bakery_shop_featured    - 20
-         * @hooked bakery_shop_welcome     - 30
-         * @hooked bakery_shop_products    - 40 
-         * @hooked bakery_shop_cta         - 60
-         * @hooked bakery_shop_team        - 80
-         * @hooked bakery_shop_testimonial - 90
-         * @hooked bakery_shop_blog        - 95 
-        */
-        do_action( 'bakery_shop_home_page' );
-        ?>
+get_header();
+?>
+<body>
    
-    <?php        
-    }else {
-        include( get_page_template() );
-    }
+    <div>
+        <nav class="navbar navbar-light navbar-expand-md sticky-top navigation-clean-button" style="height:80px;background-color:#37434d;color:#ffffff;">
+            <div class="container"><a class="navbar-brand" href="index.html">BYBakery</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse"
+                    id="navcol-1">
+                    <ul class="nav navbar-nav">
+                        <li class="nav-item" role="presentation"><a class="nav-link active" href="index.html">Home</a></li>
+                        <li class="nav-item" role="presentation"><a class="nav-link" href="Shop.html">Shop</a></li>
+                        <li class="nav-item" role="presentation"><a class="nav-link" href="Blog.html">Blog</a></li>
+                        
+                        <li class="nav-item" role="presentation"><a class="nav-link" href="contact.html">Contact</a></li>
+                        <li class="nav-item" role="presentation"><a class="nav-link" href="about.html">About</a></li>
+                    </ul>
+                    <form class="form-inline mr-auto" target="_self">
+                        <div class="form-group"><label for="search-field"><i class="fa fa-search"></i></label><input class="form-control search-field" type="search" id="search-field" name="search"></div>
+                    </form><a class="btn btn-light action-button" role="button" href="#">Action </a></div>
+            </div>
+    </nav>
+    </div>
+    <div class="carousel slide" data-ride="carousel" id="carousel-1">
+        <div class="carousel-inner" role="listbox">
+            <div class="carousel-item active">
+                <div class="jumbotron hero-nature carousel-hero">
+                    <h1 class="hero-title">Hero Nature</h1>
+                    <p class="hero-subtitle"><br>Experience The Magic Of ByBakery Shops<br><br></p>
+                    <p><a class="btn btn-primary btn-lg hero-button" role="button" href="#">Contact Us</a></p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="jumbotron hero-photography carousel-hero">
+                    <h1 class="hero-title">Hero Photography</h1>
+                    <p class="hero-subtitle">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam.</p>
+                    <p><a class="btn btn-primary btn-lg hero-button" role="button" href="#">Learn more</a></p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="jumbotron hero-technology carousel-hero">
+                    <h1 class="hero-title">Hero Technology</h1>
+                    <p class="hero-subtitle">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam.</p>
+                    <p><a class="btn btn-primary btn-lg hero-button" role="button" href="#">Learn more</a></p>
+                </div>
+            </div>
+        </div>
+        <div><a class="carousel-control-prev" href="#carousel-1" role="button" data-slide="prev"><i class="fa fa-chevron-left"></i><span class="sr-only">Previous</span></a><a class="carousel-control-next" href="#carousel-1" role="button" data-slide="next"><i class="fa fa-chevron-right"></i><span class="sr-only">Next</span></a></div>
+        <ol
+            class="carousel-indicators">
+            <li data-target="#carousel-1" data-slide-to="0" class="active"></li>
+            <li data-target="#carousel-1" data-slide-to="1"></li>
+            <li data-target="#carousel-1" data-slide-to="2"></li>
+            </ol>
+    </div>
+    <div></div>
+    <div class="features-boxed">
+        <div class="container">
+            <div class="intro">
+                <h2 class="text-center">Features </h2>
+                <p class="text-center">Nunc luctus in metus eget fringilla. Aliquam sed justo ligula. Vestibulum nibh erat, pellentesque ut laoreet vitae.</p>
+            </div>
+            <div class="row features">
+                <div class="col-sm-6 col-md-4 item">
+                    <div class="box">
+                        <div class="image-box"><img class="box-image img-responsive" src="assets/img/BYBakery-Homepic-6.jpg"></div>
+                        <div class="info-box">
+                            <h3 class="name">Works everywhere</h3>
+                            <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu.</p><a class="learn-more" href="#">Read more »</a></div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4 item">
+                    <div class="box">
+                        <div class="image-box"><img class="box-image img-responsive" src="assets/img/BYBakery-Homepic-1.jpg"></div>
+                        <div class="info-box">
+                            <h3 class="name">Works everywhere</h3>
+                            <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu.</p><a class="learn-more" href="#">Read more »</a></div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4 item">
+                    <div class="box">
+                        <div class="image-box"><img class="box-image img-responsive" src="assets/img/BYBakery-Homepic-2.jpg"></div>
+                        <div class="info-box">
+                            <h3 class="name">Works everywhere</h3>
+                            <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu.</p><a class="learn-more" href="#">Read more »</a></div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4 item">
+                    <div class="box">
+                        <div class="image-box"><img class="box-image img-responsive" src="assets/img/BYBakery-Homepic-3.jpg"></div>
+                        <div class="info-box">
+                            <h3 class="name">Works everywhere</h3>
+                            <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu.</p><a class="learn-more" href="#">&nbsp;Read more »</a></div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4 item">
+                    <div class="box">
+                        <div class="image-box"><img class="box-image img-responsive" src="assets/img/BYBakery-Homepic-4.jpg"></div>
+                        <div class="info-box">
+                            <h3 class="name">Works everywhere</h3>
+                            <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu.</p><a class="learn-more" href="#">Read more »</a></div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4 item">
+                    <div class="box">
+                        <div class="image-box"><img class="box-image img-responsive" src="assets/img/BYBakery-Homepic-5.jpg"></div>
+                        <div class="info-box">
+                            <h3 class="name">Works everywhere</h3>
+                            <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu.</p><a class="learn-more" href="#">Read more »</a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="features-boxed"></div>
+    <div class="footer-dark">
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3>Services</h3>
+                        <ul>
+                            <li><a href="/shop.html">Bread</a></li>
+                            <li><a href="/shop.html">Cake</a></li>
+                            <li><a href="#/shop.html">Cokies</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3>About</h3>
+                        <ul>
+                            <li><a href="/Contact.html">Contact</a></li>
+                            <li><a href="/shop.html">Shope</a></li>
+                            <li><a href="/about.html">About</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-6 item text">
+                        <h3>Company Name</h3>
+                        <p>Praesent sed lobortis mi. Suspendisse vel placerat ligula. Vivamus ac sem lacus. Ut vehicula rhoncus elementum. Etiam quis tristique lectus. Aliquam in arcu eget velit pulvinar dictum vel in justo.</p>
+                    </div>
+                    <div class="col item social"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-instagram"></i></a></div>
+                </div>
+                <p class="copyright">Company Name © 2017</p>
+            </div>
+        </footer>
+    </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/Contact-FormModal-Contact-Form-with-Google-Map.js"></script>
+    <script src="assets/js/WOWSlider-about-us.js"></script>
+</body>
 
-
-get_footer();
+</html>
