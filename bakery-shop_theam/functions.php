ce7545056e956757f959d140a1dc48c694064f9d
yegetaf <37799220+yegetaf@users.noreply.github.com>
2021-05-26 01:10:42 +0300

    Add files via upload
    function

--- bakery-shop_theam/functions.php
+++ bakery-shop_theam/functions.php
@@ -1,77 +1,35 @@
 <?php
-/**
- * Bakery Shop functions and definitions
- *
- * @link https://developer.wordpress.org/themes/basics/theme-functions/
- *
- * @package Bakery_Shop
- */
 
-//define theme version
-if ( !defined( 'BAKERY_SHOP_THEME_VERSION' ) ) {
-	$theme_data = wp_get_theme();
-	
-	define ( 'BAKERY_SHOP_THEME_VERSION', $theme_data->get( 'Version' ) );
-}
+function by_setup(){
 
-/* Declare Global Default Page ID*/
-$bakery_shop_default_page = '';
-$bakery_shop_page_array = get_pages();
-if(is_array($bakery_shop_page_array)){
-    $bakery_shop_default_page = $bakery_shop_page_array[0]->ID;
-}
+	$version = wp_get_theme()->get('version');
+	wp_enqueue_style('BYBakery', get_template_directory(),'/style.css');
+	wp_enqueue_style('bootstrap',get_template_directory(),'/assets/bootstrap/css/bootstrap.min.css',array(), '1.2.2.3', true);
+	wp_enqueue_style('fontawesome',get_template_directory(),'/assets/fonts/fontawesome-all.min.css',array(), '1.2.2.3', true);
+	wp_enqueue_style('font-awesome',get_template_directory(),'/assets/fonts/font-awesome.min.css',array(), '1.2.2.3', true);
+	wp_enqueue_style('fontawesome5-overrides',get_template_directory(),'/assets/fonts/fontawesome5-overrides.min.css',array(), '1.2.2.3', true);
+	wp_enqueue_style('Bold-BS4-Cards-with-Hover-Effect-50-1',get_template_directory(),'assets/css/Bold-BS4-Cards-with-Hover-Effect-50-1.css',array(), '1.2.2.3', true);
+	wp_enqueue_style('Bold-BS4-Cards-with-Hover-Effect-50',get_template_directory(),'https://github.com/yegetaf/bakery-shop_theam/blob/main/bakery-shop_theam/asset/css/Carousel-Hero.css',array(), '1.2.2.3', true);
+	wp_enqueue_style('Carousel-Hero',get_template_directory(),'/assets/css/Carousel-Hero.css',array(), '1.2.2.3', true);
+	wp_enqueue_style('Contact-FormModal-Contact-Form-with-Google-Map',get_template_directory(),'/assets/css/Contact-FormModal-Contact-Form-with-Google-Map.css',array(), '1.2.2.3', true);
+	wp_enqueue_style('Dark-NavBar',get_template_directory(),'/assets/css/Dark-NavBar.css',array(), '1.2.2.3', true);
+	wp_enqueue_style('Features-Boxed-Remixed-1',get_template_directory(),'/assets/css/Features-Boxed-Remixed-1.css',array(), '1.2.2.3', true);
+	wp_enqueue_style('Features-Boxed-Remixed-2',get_template_directory(),'/assets/css/Features-Boxed-Remixed-2.css',array(), '1.2.2.3', true);
+	wp_enqueue_style('Features-Boxed-Remixed-3',get_template_directory(),'/assets/css/Features-Boxed-Remixed-3.css',array(), '1.2.2.3', true);
+	wp_enqueue_style('Features-Boxed-Remixed',get_template_directory(),'/assets/css/Features-Boxed-Remixed.css',array(), '1.2.2.3', true);
+	wp_enqueue_style('Navigation-with-Button',get_template_directory(),'/assets/css/Navigation-with-Button.css',array(), '1.2.2.3', true);
+	wp_enqueue_style('Search-Input-responsive',get_template_directory(),'/assets/css/Search-Input-responsive.css',array(), '1.2.2.3', true);
+	wp_enqueue_style('styles',get_template_directory(),'/assets/css/styles.css',array(), '1.2.2.3', true);
+	wp_enqueue_style('WOWSlider-about-us-1',get_template_directory(),'/assets/css/WOWSlider-about-us-1.css',array(), '1.2.3', true);
+	wp_enqueue_style('WOWSlider-about-us-2',get_stylesheet(),'/assets/css/WOWSlider-about-us-2.css',array(), '2.2.3', true);
+	wp_enqueue_style('WOWSlider-about-us',get_template_directory(),'/assets/css/WOWSlider-about-us.css',array(), '1.2.3', true);
+	}
+ 
+add_action('wp_enqueue_scripts','by_setup');
 
-/* Declare Global Default Post ID*/
-$bakery_shop_default_post = '';
-$bakery_shop_post_array = get_posts();
-if(is_array($bakery_shop_post_array)){
-    $bakery_shop_default_post = $bakery_shop_post_array[0]->ID;
-}
+function by_init(){
 
-/**
- * Load Jetpack compatibility file.
- */
-if ( defined( 'JETPACK__VERSION' ) ) {
-	require get_template_directory() . '/inc/jetpack.php';
 }
 
 
-/**
- * Implement the Custom functions.
- */
-require get_template_directory() . '/inc/custom-functions.php';
-
-/**
- * Implement the WordPress Hooks.
- */
-require get_template_directory() . '/inc/wp-hooks.php';
-
-/**
- * Custom template function for this theme.
- */
-require get_template_directory() . '/inc/template-functions.php';
-
-/**
- * Custom template hooks for this theme.
- */
-require get_template_directory() . '/inc/template-hooks.php';
-
-/**
- * Custom functions that act independently of the theme templates.
- */
-require get_template_directory() . '/inc/extras.php';
-
-/**
- * Customizer additions.
- */
-require get_template_directory() . '/inc/customizer/customizer.php';
-
-/**
- * Load plugin for right and no sidebar
- */
-require get_template_directory() . '/inc/metabox.php';
-
-/**
- * Load widgets.
- */
-require get_template_directory() . '/inc/widgets/widgets.php';
+?>
