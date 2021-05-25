4982227b6a525045780b131ab2beeb0313e0644f
yegetaf <37799220+yegetaf@users.noreply.github.com>
2021-05-26 01:09:30 +0300

    Add files via upload
    header

--- bakery-shop_theam/header.php
+++ bakery-shop_theam/header.php
@@ -1,44 +1,65 @@
-<?php
-/**
- * The header for our theme
- *
- * This is the template that displays all of the <head> section and everything up until <div id="content">
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package Bakery_Shop
- */
-
-?>
-<!doctype html>
-<html <?php language_attributes(); ?>>
+<!DOCTYPE html>
+<html>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="http://gmpg.org/xfn/11">
-
-	<?php wp_head(); ?>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>BYBakery</title>
+    
+    <?php
+    wp_head();
+    ?>
 </head>
-
-<body <?php body_class(); ?>>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'bakery-shop' ); ?></a>
-
-	<?php
-		/**
-		 * @hooked bakery_shop_header_start  - 20 
-		 * @hooked bakery_shop_header_bottom - 40 
-		 * @hooked bakery_shop_header_menu 	 - 50 
-		 * @hooked bakery_shop_header_end 	 - 60 
-		 * @hooked bakery_shop_slider_cb
-		 */
-
-		do_action( 'bakery_shop_header' );
-		do_action( 'bakery_shop_slider' ); 
-
-		/**
-		 * bakery_shop Content
-		 * 
-		 * @see bakery_shop_content_start
-		*/
-		do_action( 'bakery_shop_before_content' );
+<body>
+   
+    <div>
+        <nav class="navbar navbar-light navbar-expand-md sticky-top navigation-clean-button" style="height:80px;background-color:#37434d;color:#ffffff;">
+            <div class="container"><a class="navbar-brand" href="index.html">BYBakery</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse"
+                    id="navcol-1">
+                     <ul class="nav navbar-nav">
+                        <li class="nav-item" role="presentation"><a class="nav-link active" href="index.html">Home</a></li>
+                        <li class="nav-item" role="presentation"><a class="nav-link" href="Shop.html">Shop</a></li>
+                        <li class="nav-item" role="presentation"><a class="nav-link" href="Blog.html">Blog</a></li>
+                        
+                        <li class="nav-item" role="presentation"><a class="nav-link" href="contact.html">Contact</a></li>
+                        <li class="nav-item" role="presentation"><a class="nav-link" href="about.html">About</a></li>
+                    </ul>
+                    <form class="form-inline mr-auto" target="_self">
+                        <div class="form-group"><label for="search-field"><i class="fa fa-search"></i></label><input class="form-control search-field" type="search" id="search-field" name="search"></div>
+                    </form><a class="btn btn-light action-button" role="button" href="#">Action </a></div>
+            </div>
+    </nav>
+    </div>
+    <div class="carousel slide" data-ride="carousel" id="carousel-1">
+        <div class="carousel-inner" role="listbox">
+            <div class="carousel-item active">
+                <div class="jumbotron hero-nature carousel-hero">
+                    <h1 class="hero-title">Hero Nature</h1>
+                    <p class="hero-subtitle"><br>Experience The Magic Of ByBakery Shops<br><br></p>
+                    <p><a class="btn btn-primary btn-lg hero-button" role="button" href="#">Contact Us</a></p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="jumbotron hero-photography carousel-hero">
+                    <h1 class="hero-title">Hero Photography</h1>
+                    <p class="hero-subtitle">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam.</p>
+                    <p><a class="btn btn-primary btn-lg hero-button" role="button" href="#">Learn more</a></p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="jumbotron hero-technology carousel-hero">
+                    <h1 class="hero-title">Hero Technology</h1>
+                    <p class="hero-subtitle">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam.</p>
+                    <p><a class="btn btn-primary btn-lg hero-button" role="button" href="#">Learn more</a></p>
+                </div>
+            </div>
+        </div>
+        <div><a class="carousel-control-prev" href="#carousel-1" role="button" data-slide="prev"><i class="fa fa-chevron-left"></i><span class="sr-only">Previous</span></a><a class="carousel-control-next" href="#carousel-1" role="button" data-slide="next"><i class="fa fa-chevron-right"></i><span class="sr-only">Next</span></a></div>
+        <ol
+            class="carousel-indicators">
+            <li data-target="#carousel-1" data-slide-to="0" class="active"></li>
+            <li data-target="#carousel-1" data-slide-to="1"></li>
+            <li data-target="#carousel-1" data-slide-to="2"></li>
+            </ol>
+    </div>
+    <div></div>
